ee5bf55902f09878fb360f7e83fca1a7225a90fb
Silas Lenz <silas.lenz@gmail.com>
2015-01-15 22:02:14 +0100

    anaconda

--- ingress.py
+++ ingress.py
@@ -1,18 +1,24 @@
-import sys, time
+import sys
+import time
 from multiprocessing import Process
-ops_button_corner=(151,636)
+ops_button_corner = (151, 636)
+
+
 def find_line(rgb_im):
-    if rgb_im[ops_button_corner][0]>200 and rgb_im[ops_button_corner][1]>200 and rgb_im[ops_button_corner][2]>200:
-        for y  in range(700,1150):
-            for x in range(650,680):
-                b, g, r = rgb_im[y,x]
-                #print r,g,b
+    if rgb_im[ops_button_corner][0] > 200 and rgb_im[ops_button_corner][1] > 200 and rgb_im[ops_button_corner][2] > 200:
+        for y in range(700, 1150):
+            for x in range(650, 680):
+                b, g, r = rgb_im[y, x]
+                # print r,g,b
                 #print (rgb_im[y,x-500]==rgb_im[y,x]).all()
-                #print b
-                if (rgb_im[y,x-500]==rgb_im[y,x]).all() and b>200 and g>150 and r<50:
-                    return (x,y)
+                # print b
+                if (rgb_im[y, x - 500] == rgb_im[y, x]).all() and b > 200 and g > 150 and r < 50:
+                    return (x, y)
+
+
 def go_through_frames(fn):
-    import cv2,time
+    import cv2
+    import time
     f = open("imfile.txt", "w")
     cap = cv2.VideoCapture("./screen.mp4")
     while not cap.isOpened():
@@ -21,22 +27,22 @@ def go_through_frames(fn):
         print ("Wait for the header")
 
     pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame+200)
+    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame + 200)
     while True:
         pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame+9)
+        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame + 9)
         flag, frame = cap.read()
 
         if flag:
             # The frame is ready and already captured
-            if (fn(frame))!=None:
+            if (fn(frame)) is not None:
                 print (fn(frame))
             cv2.imshow('video', frame)
             pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-            print (str(pos_frame)+" frames")
+            print (str(pos_frame) + " frames")
         else:
             # The next frame is not ready, so we try to read it again
-            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame - 1)
             print ("frame is not ready")
             # It is better to wait for a while for the next frame to be ready
             cv2.waitKey(1000)
